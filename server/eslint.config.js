2a338b6f78cca7ba4b6eb23f783b8e4721cd9071
Smart Leads Developer <dev@smartleads.local>
2026-05-16 17:43:32 +0530

    chore(server): exclude test files from ESLint type checking

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -5,7 +5,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['dist/**', 'node_modules/**', 'src/**/__tests__/**'],
   },
   {
     files: ['src/**/*.ts'],
